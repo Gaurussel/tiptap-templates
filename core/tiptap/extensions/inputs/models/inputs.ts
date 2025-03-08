db07f0707f26bb5a5e1a52128fbc18e29646d814
Nikita Voronov <nikita.voronov@ics-it.ru>
2025-03-08 21:36:10 +0300

    rm trash

--- core/tiptap/extensions/inputs/models/inputs.ts
+++ core/tiptap/extensions/inputs/models/inputs.ts
@@ -1,19 +1,21 @@
 import { Node } from "@tiptap/core";
-import { InputType, InputComponents, InputContent } from "./types";
+import { InputType, InputComponents } from "./types";
 import { ReactNodeViewRenderer } from "@tiptap/react";
 
 const getInputsModels = (): Node[] => {
 	return Array.from(Object.entries(InputType)).map(([key, value]) => {
-		const inputContent = InputContent[key as keyof typeof InputContent];
+		const inputContent = InputType[key];
 		const content = inputContent ? `paragraph ${inputContent}` : "paragraph";
 		const name = `${value}Input`;
+		const ReactComponent = InputComponents[value];
+
 		return Node.create({
 			name,
 			group: "block inputs",
 			content,
 
 			addNodeView() {
-				return ReactNodeViewRenderer(InputComponents[value]);
+				return ReactNodeViewRenderer(ReactComponent);
 			},
 		});
 	});
--- core/tiptap/extensions/inputs/models/types.ts
+++ core/tiptap/extensions/inputs/models/types.ts
@@ -1,16 +1,11 @@
 import type { ComponentType } from "react";
+import type { NodeViewProps } from "@tiptap/core";
 import DateInputComponent from "../components/dateInputComponent";
 import ParagraphInputComponent from "../components/paragraphInputComponent";
-import type { NodeViewProps } from "@tiptap/core";
 
 export enum InputType {
 	Paragraph = "paragraph",
 	Date = "date",
-}
-
-export enum InputContent {
-	Paragraph = "paragraph",
-	Date = "paragraph",
 }
 
 export const InputComponents: Record<InputType, ComponentType<NodeViewProps>> = {
